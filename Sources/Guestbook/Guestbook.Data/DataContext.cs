8fda4217480fe6c8ce7b2d2b17abbebf2270a000
Denis Kurilenko <kurilenkodenis@gmail.com>
2015-01-30 00:44:14 +0200

    add entity framework dependency  to data layer project and create data context.

---
+++ Sources/Guestbook/Guestbook.Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿namespace Guestbook.Data
+{
+    using System.Data.Entity;
+
+    internal class DataContext : DbContext
+    {
+        /// <summary>
+        /// Default <see cref="DataContext"/> constructor uses 'Guestbook' connection string
+        /// which should be provided in Web.config or App.config.
+        /// </summary>
+        public DataContext() : base("name=Guestbook")
+        {
+        }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+    }
+}
