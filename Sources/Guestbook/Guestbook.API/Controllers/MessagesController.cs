a0025ec8b6f7ecf3c103208b28c00943c499c6fa
Denis Kurilenko <kurilenkodenis@gmail.com>
2015-01-30 02:36:43 +0200

    setup web api and create messages controller

---
+++ Sources/Guestbook/Guestbook.API/Controllers/MessagesController.cs
@@ -0,0 +1,8 @@
+﻿namespace Guestbook.API.Controllers
+{
+    using System.Web.Http;
+
+    public class MessagesController : ApiController
+    {
+    }
+}
---
+++ Sources/Guestbook/Guestbook.API/Startup.cs
@@ -0,0 +1,19 @@
+﻿[assembly: Microsoft.Owin.OwinStartup(typeof(Guestbook.API.Startup))]
+namespace Guestbook.API
+{
+    using System.Web.Http;
+    using Owin;
+    
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute("def", "api/{Controller}");
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            app.UseWebApi(config);
+        }
+    }
+}
